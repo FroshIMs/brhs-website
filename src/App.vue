c926bed75ba67838e3436904004d12bf959f099d
froshims <lazarusgrey37@gmail.com>
2020-08-06 15:52:09 -0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,17 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <div class="uk-card uk-card-default uk-card-body uk-width-1-2@m">
+      <h3 class="uk-card-title">Default</h3>
+      <p>Lorem ipsum <a href="#">dolor</a> sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    </div>
+    <!-- <router-view/> -->
   </div>
 </template>
 
 <style lang="less">
+@import "../node_modules/uikit/src/less/uikit.less";
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
