c88751199277a64d6a37526ed1e36a316aecefa9
froshims <lazarusgrey37@gmail.com>
2020-08-09 18:43:51 -0700

    Updates home page

---
+++ dist/precache-manifest.cd499b5ebdd7a7c77c093fcf27caca53.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8ffa14772e5aeeb85ed",
+    "url": "/css/app.bb07be0c.css"
+  },
+  {
+    "revision": "20f62dcf7f4ecd8c89729e6dd23165bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ff359eb374abbc2a567b",
+    "url": "/js/about.09f9ab45.js"
+  },
+  {
+    "revision": "f8ffa14772e5aeeb85ed",
+    "url": "/js/app.173bcaa7.js"
+  },
+  {
+    "revision": "751e60b5d632ef97334f",
+    "url": "/js/chunk-vendors.62c92950.js"
+  },
+  {
+    "revision": "16242935f7dba09d4f865761577d8ff9",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
