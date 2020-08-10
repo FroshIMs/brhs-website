665a9440cf8eb1a4c627a09799ecc08ff9b6e605
froshims <lazarusgrey37@gmail.com>
2020-08-09 18:58:45 -0700

    Actual Project

--- src/App.vue
+++ src/App.vue
@@ -4,24 +4,12 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <div class="uk-card uk-card-default uk-card-body uk-width-1-2@m">
-      <h3 class="uk-card-title">Default</h3>
-      <p>Lorem ipsum <a href="#">dolor</a> sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-    </div>
-    <!-- <router-view/> -->
+     <router-view/>
   </div>
 </template>
 
 <style lang="less">
 @import "../node_modules/uikit/src/less/uikit.less";
-
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
 #nav {
   padding: 30px;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,112 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="uk-container">
+    <div class="uk-section uk-section-default">
+      <div class="uk-container">
+
+        <h3>Section Default</h3>
+
+        <div class="uk-grid-match uk-child-width-1-3@m" uk-grid>
+          <div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+          </div>
+          <div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+          </div>
+          <div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+    <div class="uk-section uk-section-muted">
+      <div class="uk-container">
+
+        <h3>Section Muted</h3>
+
+        <div class="uk-grid-match uk-child-width-1-3@m" uk-grid>
+          <div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+          </div>
+          <div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+          </div>
+          <div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+    <div class="uk-section uk-section-primary uk-light">
+      <div class="uk-container">
+
+        <h3>Section Primary</h3>
+
+        <div class="uk-grid-match uk-child-width-1-3@m" uk-grid>
+          <div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+          </div>
+          <div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+          </div>
+          <div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+    <div class="uk-section uk-section-secondary uk-light">
+      <div class="uk-container">
+
+        <h3>Section Secondary</h3>
+
+        <div class="uk-grid-match uk-child-width-1-3@m" uk-grid>
+          <div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+          </div>
+          <div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+          </div>
+          <div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+    <div class="uk-section-default">
+      <div class="uk-section uk-light uk-background-cover" style="background-image: url(https://getuikit.com/docs/images/dark.jpg)">
+        <div class="uk-container">
+
+          <h3>Section with Images</h3>
+
+          <div class="uk-grid-match uk-child-width-1-3@m" uk-grid>
+            <div>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+            </div>
+            <div>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+            </div>
+            <div>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
 }
 </script>
